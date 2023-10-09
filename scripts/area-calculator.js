761c90ede1bb3f837e4b28205207c1082aa1e518
tasfiameher <tasfiameher060@gmail.com>
2023-10-09 22:22:35 +0600

    parallelogram and ellipse added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -23,12 +23,12 @@ function calculateRectangleArea() {
     const widthField = document.getElementById('rectangle-width');
     const widthValueText = widthField.value;
     const width = parseFloat(widthValueText);
-    console.log(width);
+    // console.log(width);
     // get rectangle length
     const lengthField = document.getElementById('rectangle-length');
     const lengthValueText = lengthField.value;
     const length = parseFloat(lengthValueText);
-    console.log(length);
+    // console.log(length);
 
     const rectangleArea = width * length;
 
@@ -39,3 +39,38 @@ function calculateRectangleArea() {
 }
 
 // reusable function
+
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base');
+    // console.log(base);
+
+    const height = getInputValue('parallelogram-height');
+    // console.log(height);
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+}
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area);
+}
+
+// reusable get input value field in number
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+
+}
+
+// reusable set span,p,div etc text
+
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+
+}
